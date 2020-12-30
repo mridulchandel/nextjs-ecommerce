56ead4055093d580826ddfaa85ecba3481979413
mridulchandel <43166857+mridulchandel@users.noreply.github.com>
2020-12-30 18:16:15 +0530

    Changed base url

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "mystore145263.vercel.app/api/"
+    ? "https://mystore145263.vercel.app/api/"
     : "http://localhost:3000/api/";
 
 export default baseUrl;
